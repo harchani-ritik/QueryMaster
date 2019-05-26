c1c352874693042f5f7de0289e8e70bcea93bd7e
Ritik Harchani <harchani.ritik@gmail.com>
2019-05-27 03:41:26 +0530

    Add a Sign Out Menu Button on MainActivity

--- app/src/main/java/com/example/android/querymaster/MainActivity.java
+++ app/src/main/java/com/example/android/querymaster/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -88,5 +90,22 @@ public class MainActivity extends AppCompatActivity {
     }
     private void onSignedOutCleanup() {
         mUsername = ANONYMOUS;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
