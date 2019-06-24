46abc7372eea7337dac05141a0ea2dc138be433d
MiKinshu <mishraprateekaries@gmail.com>
2019-06-24 22:32:02 +0530

    Added navigation drawer

--- app/src/main/java/com/example/android/querymaster/MainActivity.java
+++ app/src/main/java/com/example/android/querymaster/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.android.querymaster;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -53,11 +57,26 @@ public class MainActivity extends AppCompatActivity {
     private SearchView searchView;
     private Button submitQueryButton;
     private EditText queryEditText;
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START))
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, mDrawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         getIncomingIntent();
 
