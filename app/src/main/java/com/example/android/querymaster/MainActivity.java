b16adf777f9442ee05fdb840ecafd3cdd483af2e
Ritik Harchani <harchani.ritik@gmail.com>
2019-06-26 02:32:43 +0530

    Some UI changes

--- app/src/main/java/com/example/android/querymaster/MainActivity.java
+++ app/src/main/java/com/example/android/querymaster/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.android.querymaster;
 
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
