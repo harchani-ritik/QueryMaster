7ba17864f1b80451707bb5165ade5846e802cbc1
Ritik Harchani <harchani.ritik@gmail.com>
2019-06-26 17:31:44 +0530

    Replaced Submit button with POST menu button

--- app/src/main/java/com/example/android/querymaster/Answering_Activity.java
+++ app/src/main/java/com/example/android/querymaster/Answering_Activity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,34 +35,16 @@ public class Answering_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_answering_);
         questionTextView=findViewById(R.id.questionTextView);//fill in this using intent from main activity
         answerEditText=findViewById(R.id.answerEditText);
-        Button submitButton=findViewById(R.id.submitButton);
-        Button uploadimageButton=findViewById(R.id.uploadimageButton);
-
+        Button uploadImageButton=findViewById(R.id.uploadimageButton);
 
         getIncomingIntent();
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Submitting Answer",Toast.LENGTH_SHORT).show();
-                String answer=answerEditText.getText().toString();
-                answerEditText.setText("");
-                answerArrayList.add(answer);
-                //Now this answer has to be added to ArrayList of Answers of that particular query
-                Intent myIntent=new Intent(v.getContext(),MainActivity.class);
-                myIntent.putExtra("objPosition",ObjPosition);
-                myIntent.putStringArrayListExtra("answersList",answerArrayList);
-                myIntent.putExtra("objKey",ObjKey);
-                v.getContext().startActivity(myIntent);
-            }
-        });
-
-        uploadimageButton.setOnClickListener(new View.OnClickListener() {
+        /*uploadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
-        });
+        });*/
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -95,6 +80,33 @@ public class Answering_Activity extends AppCompatActivity {
     private void setQuestionName()
     {
         questionTextView.setText(questionName);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Use to inflate the SignOut Options menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu2, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.post_menu:
+                Toast.makeText(getApplicationContext(),"Submitting Answer",Toast.LENGTH_SHORT).show();
+                String answer=answerEditText.getText().toString();
+                answerEditText.setText("");
+                answerArrayList.add(answer);
+                //Now this answer has to be added to ArrayList of Answers of that particular query
+                Intent myIntent=new Intent(this,MainActivity.class);
+                myIntent.putExtra("objPosition",ObjPosition);
+                myIntent.putStringArrayListExtra("answersList",answerArrayList);
+                myIntent.putExtra("objKey",ObjKey);
+                startActivity(myIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
